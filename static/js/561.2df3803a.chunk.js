15c4d8f8c7286599f746a80a318021b899c280e1
Tatianayarchenko <93308730+Tatianayarchenko@users.noreply.github.com>
2022-09-07 10:48:39 +0000

    Deploying to gh-pages from @ Tatianayarchenko/goit-react-hw-08-phonebook@005dd489e2a957d2a29350c2e8d016ff72265544 🚀

--- static/js/561.2df3803a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[561],{6561:function(e,o,t){t.r(o);var c=t(184);o.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"Phonebook"}),(0,c.jsx)("p",{children:"Welcome to the phonebook"}),(0,c.jsx)("p",{children:"Aplication for your contacts"})]})}}}]);
-//# sourceMappingURL=561.2df3803a.chunk.js.map
